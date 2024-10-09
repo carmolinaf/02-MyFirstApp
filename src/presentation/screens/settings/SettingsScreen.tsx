a9e97b943a1f6ed6d0940b133808fedde536a09e
carmoliaflPersonal <65577233+carmolinaf@users.noreply.github.com>
2024-10-08 22:11:53 -0300

    video 108

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { globalStyles } from '../../theme/theme'
+import { PrimaryButton } from '../../components/shared/PrimaryButton'
+import { NavigationRouteContext, StackActions, useNavigation } from '@react-navigation/native'
 
 export const SettingsScreen = () => {
+
+  const navigator = useNavigation();
   return (
-    <View>
+    <View style= { globalStyles.container }>
+
     <Text> SettingsScreen.tsx</Text>
+
+    <PrimaryButton
+    label= "Regresar"
+    onPress= { ()=> navigator.goBack() }
+    />
+
+<PrimaryButton
+    label= "Regresar al home"
+    onPress= { ()=> navigator.dispatch( StackActions.popToTop() ) }
+    />
+
+
     </View>
   )
 }
