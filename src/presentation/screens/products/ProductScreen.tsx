b1a07e8994655471c1de7b8dcff07645e40a15fd
carmoliaflPersonal <65577233+carmolinaf@users.noreply.github.com>
2024-10-08 21:54:33 -0300

    video 107
    se agregan los cambios del video 107

--- src/presentation/screens/products/ProductScreen.tsx
+++ src/presentation/screens/products/ProductScreen.tsx
@@ -1,11 +1,34 @@
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native"
 import { Text, View } from "react-native"
+import { RootStackParams } from "../../routes/StackNavigator";
+import { globalStyles } from "../../theme/theme";
+import { useEffect } from "react";
 
-export const ProductScreen = (props:any) => {
+export const ProductScreen = () => {
  
-  console.log(props);
+ const params = useRoute<RouteProp<RootStackParams, 'Product'>>().params;
+ const navigation = useNavigation();
+
+ useEffect( () =>{
+  navigation.setOptions({
+    title: params.name
+
+  })
+
+ }, [] )
+ 
+ console.log(params);
+
   return (
-    <View>
+    <View style= {globalStyles.container}>
         <Text> ProductScreen</Text>
+        <Text style =  {{
+          fontSize: 20,
+          textAlign: 'center',
+          marginTop: 20
+        }}> 
+        { params.id } - { params.name }
+        </Text>
     </View>
   )
 }
