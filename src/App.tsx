72feda724d4dc4faff5727992481fc86d90512d1
carmoliaflPersonal <65577233+carmolinaf@users.noreply.github.com>
2024-10-08 22:55:54 -0300

    120-BottomTabNavigator

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native'
 import { Text, View } from 'react-native'
 import { StackNavigator } from './presentation/routes/StackNavigator';
+import { BottomTabNavigator } from './presentation/routes/BottomTabsNavigator';
 
 export const App = () => {
   return (
@@ -10,7 +11,10 @@ export const App = () => {
 
 
 
-        <StackNavigator />
+        {/*<StackNavigator />*/}
+
+        <BottomTabNavigator />
+
 
 
 
