8a9e85145d4caf3e93894fed5a5c29318e5f7296
carmoliaflPersonal <65577233+carmolinaf@users.noreply.github.com>
2024-10-09 23:19:45 -0300

    paquete de iconos listos

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,15 +1,19 @@
+import React from 'react';
 import { Text, View } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
-
 
 
 export const Tab1Screen = () => {
   return (
     <View>
       <Text>Tab1Screen</Text>
+      <Icon name="add-outline" size={30} color="#900" />
 
-      
+<Icon name="airplane-outline" size={30} />
+
     </View>
   )
 }
+
+
