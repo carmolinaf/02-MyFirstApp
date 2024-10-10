0a112175ab29de98870381f731629cfb103fdf01
carmoliaflPersonal <65577233+carmolinaf@users.noreply.github.com>
2024-10-09 23:29:59 -0300

    se crea un componente Icono
    de esta forma se reutiliza de la misma forma que se reutiliza una funcion

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-import Icon from 'react-native-vector-icons/Ionicons';
+//import Icon from 'react-native-vector-icons/Ionicons';
+import { IonIcon } from '../../components/shared/IonIcon';
 
 
 export const Tab1Screen = () => {
   return (
     <View>
       <Text>Tab1Screen</Text>
-      <Icon name="add-outline" size={30} color="#900" />
+      {/*<Icon name="add-outline" size={30} color="#900" />
 
-<Icon name="airplane-outline" size={30} />
+      <Icon name="airplane-outline" size={30} />*/}
+      <IonIcon name='rocket-outline' />
 
     </View>
   )
